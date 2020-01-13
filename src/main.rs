e2cd92b33a1b516c8605b20440e814ee43b168b6
Guillaume Balaine <igosuki@gmail.com>
2020-01-13 09:24:42 +0100

    Make processes more configurable, next step is making processes restart after config changes

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ extern crate lazy_static;
 extern crate uuid;
 #[macro_use] extern crate log;
 #[macro_use] extern crate byte_unit;
+extern crate config;
 
 use crate::coinnect_rt::bitstamp::BitstampCreds;
 use crate::coinnect_rt::coinnect::Coinnect;
@@ -43,13 +44,51 @@ use coinnect_rt::exchange_bot::{DefaultWsActor, ExchangeBot};
 use coinnect_rt::bittrex::BittrexCreds;
 use byte_unit::Byte;
 use chrono::{Duration};
-
+use notify::{RecommendedWatcher, DebouncedEvent, Watcher, RecursiveMode};
+use std::sync::mpsc::channel;
+use std::sync::RwLock;
+use std::env::var;
 pub mod avro_gen;
 pub mod handlers;
 pub mod api;
+use config::*;
+
+lazy_static! {
+    static ref CONFIG_FILE: String = {
+        let trader_env : String = std::env::var("TRADER_ENV").unwrap_or("development".to_string());
+        format!("config/{}.yaml", trader_env)
+    };
+}
+
+lazy_static! {
+    static ref SETTINGS: RwLock<Config> = RwLock::new({
+        let mut settings = Config::default();
+        settings.merge(File::with_name(&CONFIG_FILE)).unwrap();
+
+        settings
+    });
+}
+
+fn show() {
+    println!(" * Settings :: \n\x1b[31m{:?}\x1b[0m",
+             SETTINGS
+                 .read()
+                 .unwrap()
+                 .clone()
+                 .try_into::<HashMap<String, String>>()
+                 .unwrap());
+}
 
 fn main() -> io::Result<()> {
     env_logger::init();
+
+    // Create a channel to receive the events.
+    let (tx, rx) = channel();
+    let mut watcher: RecommendedWatcher = Watcher::new(tx, std::time::Duration::from_secs(2)).unwrap();
+
+    watcher
+        .watch(&*CONFIG_FILE, RecursiveMode::NonRecursive)
+        .unwrap();
 
     let sys = System::new("websocket-client");
 
@@ -59,10 +98,14 @@ fn main() -> io::Result<()> {
         let fa = AvroFileActor::create(|ctx| {
             let dir = Path::new("./data").clone();
             fs::create_dir_all(&dir).unwrap();
-            let max_file_size = Byte::from_str("50 Mo").unwrap();
+            let max_file_size = Byte::from_str("50 Mb");
+            if max_file_size.is_err() {
+                debug!("Wrong max file size");
+                std::process::exit(1);
+            }
             AvroFileActor::new(&FileActorOptions {
                 base_dir: dir.to_str().unwrap().to_string(),
-                max_file_size: max_file_size.get_bytes() as u64,
+                max_file_size: max_file_size.unwrap().get_bytes() as u64,
                 max_file_time: Duration::seconds(1800),
                 partitioner: handlers::liveEventPartitioner,
             })
