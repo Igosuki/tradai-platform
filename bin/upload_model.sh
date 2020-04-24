b2cd1d316dcfc31c4dc6140970f5b5240895862d
Guillaume Balaine <igosuki@gmail.com>
2020-04-24 22:34:50 +0200

    Change the model upload path

--- bin/upload_model.sh
+++ bin/upload_model.sh
@@ -2,4 +2,4 @@
 
 BASEDIR=$(dirname "$0")
 
-scp -r -i ~/.ssh/btc "$BASEDIR/data/naive_pair_trading_model_$1_$2" root@167.71.204.126:/root/database/naive_pair_trading
+scp -r -i ~/.ssh/btc "$BASEDIR/data/naive_pair_trading_$1_$2/naive_pair_trading_model_$1_$2" root@167.71.204.126:/root/database/naive_pair_trading_$1_$2
