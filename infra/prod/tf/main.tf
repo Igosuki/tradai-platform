0d38f106ea9a907d5152fd1c86038f28dee79818
Guillaume Balaine <igosuki@gmail.com>
2020-04-24 12:56:45 +0200

    Add a separate machine for monitoring and firewall rules for comm since there is no vpc

--- infra/prod/tf/main.tf
+++ infra/prod/tf/main.tf
@@ -2,8 +2,8 @@
 provider "digitalocean" {
   token = var.do_token
 }
-data "digitalocean_project" "bitcoins" {
-}
+data "digitalocean_project" "bitcoins" {}
+
 resource "digitalocean_droplet" "trader" {
   image  = "ubuntu-18-04-x64"
   name   = "trader"
@@ -15,4 +15,65 @@ resource "digitalocean_droplet" "trader" {
   tags = ["trader", "prod"]
 }
 
+resource "digitalocean_droplet" "monitoring" {
+  image  = "ubuntu-18-04-x64"
+  name   = "monitoring"
+  region = "sgp1"
+  size   = "s-1vcpu-1gb"
+  backups = true
+  ipv6 = true
+  ssh_keys = [26568141]
+  tags = ["monitoring", "prod"]
+}
 
+resource "digitalocean_firewall" "monitoring" {
+  name = "prometheus"
+
+  droplet_ids = [digitalocean_droplet.monitoring.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9090"
+    source_addresses = ["::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+resource "digitalocean_firewall" "trader" {
+  name = "trader-monitoring"
+
+  droplet_ids = [digitalocean_droplet.monitoring.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["::/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9091"
+    source_addresses = [digitalocean_droplet.monitoring.ipv4_address]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "9100"
+    source_addresses = [digitalocean_droplet.monitoring.ipv4_address]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
