a32f2c4bf0138d74b051b2c218cee9b96bd12380
Guillaume Balaine <igosuki@gmail.com>
2020-04-29 20:38:00 +0200

    Save 'operations' instead of positions, and reload the previously saved MovingState when instantiating a new one

--- strategies/src/query.rs
+++ strategies/src/query.rs
@@ -1,15 +1,16 @@
 use super::naive_pair_trading::state::Position;
 use super::naive_pair_trading::state::PositionKind;
+use crate::naive_pair_trading::state::Operation;
 use actix_derive::{Message, MessageResponse};
 
 #[derive(Deserialize, Serialize, MessageResponse)]
 #[serde(tag = "type")]
 pub enum DataResult {
-    Positions(Vec<Position>),
+    Operations(Vec<Operation>),
 }
 
 #[derive(Deserialize, Serialize, Message)]
 #[rtype(result = "Option<DataResult>")]
 pub enum DataQuery {
-    Positions,
+    Operations,
 }
