a003bd1e04d725f0877a0c48eab1d1a21aef91d7
Guillaume Balaine <igosuki@gmail.com>
2020-06-29 10:32:20 +0200

    cli tool to dump db content

---
+++ db/src/main.rs
@@ -0,0 +1,20 @@
+use db::Db;
+use std::env;
+
+const DUMP: &str = "dump";
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    let db = Db::new(&args[2], args[3].clone());
+    let x: &str = &args[1];
+    println!("{}", x);
+    match x {
+        DUMP => {
+            let strings = db.all();
+            for string in strings {
+                println!("{}", string);
+            }
+        }
+        _ => {}
+    }
+}
