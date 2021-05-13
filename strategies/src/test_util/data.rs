797badc40041429a401f6292bcded67571b3db71
Guillaume Balaine <igosuki@gmail.com>
2021-05-13 17:13:13 +0200

    Use a ramfs directory for testing

--- strategies/src/test_util/data.rs
+++ strategies/src/test_util/data.rs
@@ -1,7 +1,8 @@
 #[cfg(test)]
 pub mod data_test_util {
     pub fn test_dir() -> String {
-        let root = tempdir::TempDir::new("test_data2").unwrap();
+        let basedir = std::env::var("BITCOINS_TEST_RAMFS_DIR").unwrap_or_else(|_| "/media/ramdisk".to_string());
+        let root = tempdir::TempDir::new(&format!("{}/test_data", basedir)).unwrap();
         let buf = root.into_path();
         let path = buf.to_str().unwrap();
         path.to_string()
