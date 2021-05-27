d370d10250377ff4e34c1e8bfe7c573d4b9c90d4
Guillaume Balaine <igosuki@gmail.com>
2021-05-27 20:38:32 +0200

    Implement a 'storage' interface in the db crate, with rocksdb being the default implementation

---
+++ db/src/error.rs
@@ -0,0 +1,11 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error("rkv error")]
+    RocksDbError(#[from] rocksdb::Error),
+    #[error("record not found {0}")]
+    NotFoundError(String),
+}
+
+pub type Result<T> = core::result::Result<T, Error>;
