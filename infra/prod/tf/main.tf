59406a70e4395896845026efd8e7d78fedb72823
Guillaume Balaine <igosuki@gmail.com>
2021-06-29 18:10:45 +0200

    Remove the trader droplet

--- infra/prod/tf/main.tf
+++ infra/prod/tf/main.tf
@@ -24,17 +24,6 @@ resource "digitalocean_droplet" "trader2" {
   tags     = ["trader", "prod"]
 }
 
-resource "digitalocean_droplet" "trader" {
-  image    = "ubuntu-18-04-x64"
-  name     = "trader"
-  region   = "sgp1"
-  size     = "s-1vcpu-1gb"
-  backups  = true
-  ipv6     = true
-  ssh_keys = [26568141]
-  tags     = ["trader", "prod"]
-}
-
 resource "digitalocean_droplet" "monitoring" {
   image    = "ubuntu-18-04-x64"
   name     = "monitoring"
