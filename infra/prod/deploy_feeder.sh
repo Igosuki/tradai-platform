df50b1ed139aeb91196c9dbf01d37682fc82fdbf
Guillaume Balaine <igosuki@gmail.com>
2021-07-05 04:20:49 +0200

    allow configuring options for the feeder service such as min age of files to be uploaded and configuration file name

--- infra/prod/deploy_feeder.sh
+++ infra/prod/deploy_feeder.sh
@@ -2,4 +2,4 @@
 
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-ansible-playbook -i inventory.yml playbooks/feeder_deploy.yml
+ansible-playbook -i inventory.yml playbooks/feeder_deploy.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml"
