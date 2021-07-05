5481c8a127b5a310bd1bdeba63697710df11b2dd
Guillaume Balaine <igosuki@gmail.com>
2021-07-05 15:06:10 +0200

    serde json arbitrary precision breaks production

--- models/src/avro_gen/models.rs
+++ models/src/avro_gen/models.rs
@@ -1,6 +1,40 @@
-use avro_rs::schema::Schema;
-use lazy_static;
 use serde::{Deserialize, Serialize};
+use lazy_static;
+use avro_rs::schema::Schema;
+
+
+lazy_static! {
+    pub static ref ORDERBOOK_SCHEMA : Schema = Schema::parse_str("{\"type\":\"record\",\"name\":\"Orderbook\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"asks\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}},{\"name\":\"bids\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}}]}").unwrap();
+}
+
+#[serde(default)]
+#[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
+pub struct Orderbook {
+    pub event_ms: i64,
+    pub pair: String,
+    pub asks: Vec<Vec<f64>>,
+    pub bids: Vec<Vec<f64>>,
+}
+
+impl Default for Orderbook {
+    fn default() -> Orderbook {
+        Orderbook {
+            event_ms: 0,
+            pair: String::default(),
+            asks: vec![],
+            bids: vec![],
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Deserialize, Serialize)]
+pub enum TradeType {
+    #[serde(rename = "BUY")]
+    Buy,
+    #[serde(rename = "SELL")]
+    Sell,
+}
+
 
 lazy_static! {
     pub static ref LIVEORDER_SCHEMA : Schema = Schema::parse_str("{\"type\":\"record\",\"name\":\"LiveOrder\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"amount\",\"type\":\"double\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"price\",\"type\":\"double\"},{\"name\":\"tt\",\"type\":\"int\"}]}").unwrap();
@@ -28,13 +62,6 @@ impl Default for LiveOrder {
     }
 }
 
-#[derive(Debug, PartialEq, Eq, Hash, PartialOrd, Ord, Clone, Deserialize, Serialize)]
-pub enum TradeType {
-    #[serde(rename = "BUY")]
-    Buy,
-    #[serde(rename = "SELL")]
-    Sell,
-}
 
 lazy_static! {
     pub static ref LIVETRADE_SCHEMA : Schema = Schema::parse_str("{\"type\":\"record\",\"name\":\"LiveTrade\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"amount\",\"type\":\"double\"},{\"name\":\"price\",\"type\":\"double\"},{\"name\":\"tt\",\"type\":\"int\"}]}").unwrap();
@@ -58,30 +85,6 @@ impl Default for LiveTrade {
             amount: 0.0,
             price: 0.0,
             tt: 0,
-        }
-    }
-}
-
-lazy_static! {
-    pub static ref ORDERBOOK_SCHEMA : Schema = Schema::parse_str("{\"type\":\"record\",\"name\":\"Orderbook\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"asks\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}},{\"name\":\"bids\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}}]}").unwrap();
-}
-
-#[serde(default)]
-#[derive(Debug, PartialEq, Clone, Deserialize, Serialize)]
-pub struct Orderbook {
-    pub event_ms: i64,
-    pub pair: String,
-    pub asks: Vec<Vec<f64>>,
-    pub bids: Vec<Vec<f64>>,
-}
-
-impl Default for Orderbook {
-    fn default() -> Orderbook {
-        Orderbook {
-            event_ms: 0,
-            pair: String::default(),
-            asks: vec![],
-            bids: vec![],
         }
     }
 }
