077290bf38d364087837fbd4388b9966cf8cbf39
Guillaume Balaine <igosuki@gmail.com>
2021-07-28 20:31:49 +0200

    Update outdated dependencies

--- db/src/storage/rocksdb.rs
+++ db/src/storage/rocksdb.rs
@@ -2,6 +2,7 @@ use crate::error::*;
 use crate::storage::Storage;
 use rocksdb::{BoundColumnFamily, ColumnFamilyDescriptor, Direction, IteratorMode, Options, DB};
 use std::path::Path;
+use std::sync::Arc;
 
 type Bytes = Box<[u8]>;
 
@@ -37,7 +38,7 @@ impl RocksDbStorage {
         cf_opts
     }
 
-    fn cf(&self, name: &str) -> Result<BoundColumnFamily> {
+    fn cf(&self, name: &str) -> Result<Arc<BoundColumnFamily>> {
         self.inner
             .cf_handle(name.as_ref())
             .ok_or_else(|| Error::NotFound(name.to_string()))
@@ -47,13 +48,13 @@ impl RocksDbStorage {
 impl Storage for RocksDbStorage {
     fn _put(&self, table: &str, key: &[u8], value: &[u8]) -> Result<()> {
         let cf = self.cf(table)?;
-        self.inner.put_cf(cf, key, value).map_err(|e| e.into())
+        self.inner.put_cf(&cf, key, value).map_err(|e| e.into())
     }
 
     fn _get(&self, table: &str, key: &[u8]) -> Result<Vec<u8>> {
         let cf = self.cf(table)?;
         self.inner
-            .get_cf(cf, key)
+            .get_cf(&cf, key)
             .map_err(|e| e.into())
             .and_then(|r| r.ok_or_else(|| Error::NotFound(String::from_utf8(key.into()).unwrap())))
     }
@@ -61,7 +62,7 @@ impl Storage for RocksDbStorage {
     fn _get_ranged(&self, table: &str, from: &[u8]) -> Result<Vec<Bytes>> {
         let mode = IteratorMode::From(from, Direction::Forward);
         let cf = self.cf(table)?;
-        Ok(self.inner.iterator_cf(cf, mode).map(|(_k, v)| v).collect())
+        Ok(self.inner.iterator_cf(&cf, mode).map(|(_k, v)| v).collect())
     }
 
     fn _get_all(&self, table: &str) -> Result<Vec<(String, Bytes)>> {
@@ -69,19 +70,19 @@ impl Storage for RocksDbStorage {
         let cf = self.cf(table)?;
         Ok(self
             .inner
-            .iterator_cf(cf, mode)
+            .iterator_cf(&cf, mode)
             .map(|(k, v)| (String::from_utf8(k.into()).unwrap(), v))
             .collect())
     }
 
     fn _delete(&self, table: &str, key: &[u8]) -> Result<()> {
         let cf = self.cf(table)?;
-        self.inner.delete_cf(cf, key).map_err(|e| e.into())
+        self.inner.delete_cf(&cf, key).map_err(|e| e.into())
     }
 
     fn _delete_range(&self, table: &str, from: &[u8], to: &[u8]) -> Result<()> {
         let cf = self.cf(table)?;
-        self.inner.delete_range_cf(cf, from, to).map_err(|e| e.into())
+        self.inner.delete_range_cf(&cf, from, to).map_err(|e| e.into())
     }
 
     fn ensure_table(&self, name: &str) -> Result<()> {
