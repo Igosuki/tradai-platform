ba87c199e70bd45e1565a68a9fe216146285afa9
Guillaume Balaine <igosuki@gmail.com>
2021-07-30 18:52:07 +0200

    [refacto] the credentials interface changed slightly in coinnect

--- models/src/avro_gen/models.rs
+++ models/src/avro_gen/models.rs
@@ -7,6 +7,30 @@ pub enum TradeType {
 }
 
 lazy_static! {
+    pub static ref ORDERBOOK_SCHEMA : avro_rs::schema::Schema = avro_rs::schema::Schema::parse_str("{\"type\":\"record\",\"name\":\"Orderbook\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"asks\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}},{\"name\":\"bids\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}}]}").unwrap();
+}
+
+#[derive(Debug, PartialEq, Clone, serde::Deserialize, serde::Serialize)]
+#[serde(default)]
+pub struct Orderbook {
+    pub event_ms: i64,
+    pub pair: String,
+    pub asks: Vec<Vec<f64>>,
+    pub bids: Vec<Vec<f64>>,
+}
+
+impl Default for Orderbook {
+    fn default() -> Orderbook {
+        Orderbook {
+            event_ms: 0,
+            pair: String::default(),
+            asks: vec![],
+            bids: vec![],
+        }
+    }
+}
+
+lazy_static! {
     pub static ref LIVEORDER_SCHEMA : avro_rs::schema::Schema = avro_rs::schema::Schema::parse_str("{\"type\":\"record\",\"name\":\"LiveOrder\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"amount\",\"type\":\"double\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"price\",\"type\":\"double\"},{\"name\":\"tt\",\"type\":\"int\"}]}").unwrap();
 }
 
@@ -54,30 +78,6 @@ impl Default for LiveTrade {
             amount: 0.0,
             price: 0.0,
             tt: 0,
-        }
-    }
-}
-
-lazy_static! {
-    pub static ref ORDERBOOK_SCHEMA : avro_rs::schema::Schema = avro_rs::schema::Schema::parse_str("{\"type\":\"record\",\"name\":\"Orderbook\",\"fields\":[{\"name\":\"event_ms\",\"type\":\"long\"},{\"name\":\"pair\",\"type\":\"string\"},{\"name\":\"asks\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}},{\"name\":\"bids\",\"type\":{\"type\":\"array\",\"items\":{\"type\":\"array\",\"items\":\"double\"}}}]}").unwrap();
-}
-
-#[derive(Debug, PartialEq, Clone, serde::Deserialize, serde::Serialize)]
-#[serde(default)]
-pub struct Orderbook {
-    pub event_ms: i64,
-    pub pair: String,
-    pub asks: Vec<Vec<f64>>,
-    pub bids: Vec<Vec<f64>>,
-}
-
-impl Default for Orderbook {
-    fn default() -> Orderbook {
-        Orderbook {
-            event_ms: 0,
-            pair: String::default(),
-            asks: vec![],
-            bids: vec![],
         }
     }
 }
