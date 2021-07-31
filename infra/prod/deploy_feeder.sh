244c3be41dfb9cac6ee6f08f7f24f52be614f44c
Guillaume Balaine <igosuki@gmail.com>
2021-07-31 21:17:28 +0200

    [infra] deploy scripts should use the relative base directory for config files

--- infra/prod/deploy_feeder.sh
+++ infra/prod/deploy_feeder.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+BASEDIR=$(dirname "$0")
+
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-ansible-playbook -i inventory.yml playbooks/feeder_deploy.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml"
+ansible-playbook -i $BASEDIR/inventory.yml $BASEDIR/playbooks/feeder_deploy.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml"
--- infra/prod/run_books.sh
+++ infra/prod/run_books.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
+BASEDIR=$(dirname "$0")
+
 export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
 
-ansible-playbook -i inventory.yml playbooks/monitoring.yml
-ansible-playbook -i inventory.yml playbooks/trader.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml FEEDER_REMOTE_DIRECTORY=data FEEDER_DATA_EXPIRY_AGE=56d"
-ansible-playbook -i inventory.yml playbooks/trader.yml -e "INVENTORY_HOST=feeder24 FEEDER_CONFIG_FILE=feeder24_prod.yaml FEEDER_REMOTE_DIRECTORY=data24 FEEDER_DATA_MOVE_MIN_AGE=11h FEEDER_DATA_EXPIRY_AGE=7d"
+ansible-playbook -i $BASEDIR/inventory.yml $BASEDIR/playbooks/monitoring.yml
+ansible-playbook -i $BASEDIR/inventory.yml $BASEDIR/playbooks/trader.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml FEEDER_REMOTE_DIRECTORY=data FEEDER_DATA_EXPIRY_AGE=56d"
+ansible-playbook -i $BASEDIR/inventory.yml $BASEDIR/playbooks/trader.yml -e "INVENTORY_HOST=feeder24 FEEDER_CONFIG_FILE=feeder24_prod.yaml FEEDER_REMOTE_DIRECTORY=data24 FEEDER_DATA_MOVE_MIN_AGE=11h FEEDER_DATA_EXPIRY_AGE=7d"
