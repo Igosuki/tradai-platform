6cfe546b82c7e02e753e4511760d886b2542cbdc
Guillaume Balaine <igosuki@gmail.com>
2021-08-21 18:51:38 +0200

    [infra] : use --force to update roles

--- infra/prod/install_roles.sh
+++ infra/prod/install_roles.sh
@@ -2,8 +2,8 @@
 
 BASEDIR=$(dirname "$0")
 
-ansible-galaxy install cloudalchemy.prometheus -p $BASEDIR/playbooks/roles
-ansible-galaxy install cloudalchemy.node-exporter -p $BASEDIR/playbooks/roles
-ansible-galaxy install cloudalchemy.pushgateway -p $BASEDIR/playbooks/roles
-ansible-galaxy install cloudalchemy.grafana -p $BASEDIR/playbooks/roles
-ansible-galaxy install cloudalchemy.alertmanager -p $BASEDIR/playbooks/roles
+ansible-galaxy install cloudalchemy.prometheus --force -p $BASEDIR/playbooks/roles
+ansible-galaxy install cloudalchemy.node_exporter --force -p $BASEDIR/playbooks/roles
+ansible-galaxy install cloudalchemy.pushgateway --force -p $BASEDIR/playbooks/roles
+ansible-galaxy install cloudalchemy.grafana --force -p $BASEDIR/playbooks/roles
+ansible-galaxy install cloudalchemy.alertmanager --force -p $BASEDIR/playbooks/roles
