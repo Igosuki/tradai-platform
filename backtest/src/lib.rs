f171b0dd589900bbd0de6ea07eb1c423f9db6ac7
Guillaume Balaine <igosuki@gmail.com>
2021-08-26 15:55:30 +0200

    backtest: read the dataframe and derive orderbooks from the query, feeding them to strategies

--- backtest/src/lib.rs
+++ backtest/src/lib.rs
@@ -11,9 +11,12 @@ use datafusion::physical_plan::avro::AvroReadOptions;
 use datafusion::prelude::ExecutionContext;
 use parse_duration::parse;
 
+use datafusion::arrow::array::{Array, ListArray, PrimitiveArray, StructArray, TimestampMillisecondArray};
+use datafusion::arrow::datatypes::Float64Type;
 use datafusion::arrow::error::ArrowError;
 use datafusion::arrow::record_batch::RecordBatch;
 use datafusion::error::DataFusionError;
+use strategies::coinnect_types::{LiveEvent, LiveEventEnvelope, Orderbook};
 use strategies::input::partition_path;
 use strategies::order_manager::test_util::mock_manager;
 use strategies::{Channel, DbOptions, Strategy, StrategySettings};
@@ -100,6 +103,7 @@ impl Backtest {
             .into_os_string()
             .into_string()
             .unwrap();
+        eprintln!("db_path = {:?}", db_path);
         let order_manager_addr = mock_manager(&db_path);
 
         let strategy = strategies::Strategy::new(
@@ -121,6 +125,7 @@ impl Backtest {
         let chans = self.strategy.2.as_slice();
         //let mut heap = BinaryHeap::new();
         let period = self.period.clone();
+        let mut live_events = vec![];
         for chan in chans {
             match chan {
                 Channel::Orderbooks { xch, pair } => {
@@ -138,8 +143,74 @@ impl Backtest {
                             partitions.push(partition_file.as_path().to_string_lossy().to_string());
                         }
                     }
-                    let records = read_order_books_df(partitions, self.sample_rate).await?;
-                    println!("read {} records", records.len());
+                    let records = read_order_books_df(partitions, self.sample_rate, false).await?;
+                    for record_batch in records {
+                        let sa: StructArray = record_batch.into();
+                        let asks_col = sa
+                            .column_by_name("asks")
+                            .unwrap()
+                            .as_any()
+                            .downcast_ref::<ListArray>()
+                            .unwrap();
+                        let bids_col = sa
+                            .column_by_name("bids")
+                            .unwrap()
+                            .as_any()
+                            .downcast_ref::<ListArray>()
+                            .unwrap();
+                        let event_ms_col = sa
+                            .column_by_name("event_ms")
+                            .unwrap()
+                            .as_any()
+                            .downcast_ref::<TimestampMillisecondArray>()
+                            .unwrap();
+                        for i in 0..sa.len() {
+                            let mut bids = vec![];
+                            for bid in bids_col
+                                .value(i)
+                                .as_any()
+                                .downcast_ref::<ListArray>()
+                                .unwrap()
+                                .iter()
+                                .flatten()
+                            {
+                                let vals = bid
+                                    .as_any()
+                                    .downcast_ref::<PrimitiveArray<Float64Type>>()
+                                    .unwrap()
+                                    .values();
+                                bids.push((vals[0], vals[1]));
+                            }
+                            let mut asks = vec![];
+                            for ask in asks_col
+                                .value(i)
+                                .as_any()
+                                .downcast_ref::<ListArray>()
+                                .unwrap()
+                                .iter()
+                                .flatten()
+                            {
+                                let vals = ask
+                                    .as_any()
+                                    .downcast_ref::<PrimitiveArray<Float64Type>>()
+                                    .unwrap()
+                                    .values();
+                                asks.push((vals[0], vals[1]));
+                            }
+                            let i1 = event_ms_col.value(i);
+                            let orderbook = Orderbook {
+                                timestamp: i1,
+                                pair: pair.clone(),
+                                asks,
+                                bids,
+                                last_order_id: None,
+                            };
+                            live_events.push(LiveEventEnvelope {
+                                xch: *xch,
+                                e: LiveEvent::LiveOrderbook(orderbook),
+                            });
+                        }
+                    }
                 }
                 Channel::Trades { .. } => {
                     panic!("cannot yet read from trades");
@@ -149,19 +220,38 @@ impl Backtest {
                 }
             }
         }
+        for live_event in live_events {
+            if let Err(e) = self.strategy.1.send(Arc::new(live_event)).await {
+                println!("error sending event to strat : mailbox {}", e);
+            }
+        }
+        tokio::time::sleep(std::time::Duration::from_secs(10)).await;
         Ok(())
     }
 }
 
-async fn read_order_books_df(partitions: Vec<String>, sample_rate: Duration) -> Result<Vec<RecordBatch>> {
+async fn read_order_books_df(
+    partitions: Vec<String>,
+    sample_rate: Duration,
+    order_book_split_cols: bool,
+) -> Result<Vec<RecordBatch>> {
     let mut ctx = ExecutionContext::new();
     dbg!(&partitions);
     let mut records = vec![];
+    let order_book_selector = if order_book_split_cols {
+        "asks[0][0] as a1, asks[0][1] as aq1, asks[1][0] as a2, asks[1][1] as aq2, asks[2][0] as a3, asks[2][1] as aq3, asks[3][0] as a4, asks[3][1] as aq4, asks[4][0] as a5, asks[4][1] as aq5, bids[0][0] as b1, bids[0][1] as bq1, bids[1][0] as b2, bids[1][1] as bq2, bids[2][0] as b3, bids[2][1] as bq3, bids[3][0] as b4, bids[3][1] as bq4, bids[4][0] as b5, bids[4][1] as bq5"
+    } else {
+        "asks, bids"
+    };
     for partition in partitions {
         ctx.register_avro("order_books", &partition, AvroReadOptions::default())?;
-        let sql_query = format!("select event_ms, asks[0][0] as a1, asks[0][1] as aq1, asks[1][0] as a2, asks[1][1] as aq2, asks[2][0] as a3, asks[2][1] as aq3, asks[3][0] as a4, asks[3][1] as aq4, asks[4][0] as a5, asks[4][1] as aq5, bids[0][0] as b1, bids[0][1] as bq1, bids[1][0] as b2, bids[1][1] as bq2, bids[2][0] as b3, bids[2][1] as bq3, bids[3][0] as b4, bids[3][1] as bq4, bids[4][0] as b5, bids[4][1] as bq5 from
+        let sql_query = format!(
+            "select to_timestamp_millis(event_ms) as event_ms, {order_book_selector} from
    (select asks, bids, event_ms, ROW_NUMBER() OVER (PARTITION BY sample_time order by event_ms) as row_num
-    FROM (select asks, bids, event_ms / {sample_rate} as sample_time, event_ms from order_books)) where row_num = 1;", sample_rate = sample_rate.num_milliseconds());
+    FROM (select asks, bids, event_ms / {sample_rate} as sample_time, event_ms from order_books)) where row_num = 1;",
+            sample_rate = sample_rate.num_milliseconds(),
+            order_book_selector = order_book_selector
+        );
         let df = ctx.sql(&sql_query)?;
         let results = df.collect().await?;
         records.extend_from_slice(results.as_slice());
