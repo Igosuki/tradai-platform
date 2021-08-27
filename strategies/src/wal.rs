aa555a3be6bd8058bd661deb867ad3ee9cf15ad9
Guillaume Balaine <igosuki@gmail.com>
2021-08-27 16:23:36 +0200

    Ignore incompatible json records in wal

--- strategies/src/wal.rs
+++ strategies/src/wal.rs
@@ -24,35 +24,42 @@ impl Wal {
 
     pub fn get_all_compacted<T: DeserializeOwned + WalCmp>(&self) -> Result<HashMap<String, T>> {
         let mut records: HashMap<String, T> = HashMap::new();
-        self.backend.get_all::<T>(&self.table)?.into_iter().for_each(|(k, v)| {
-            let split: Vec<&str> = k.split(WAL_KEY_SEP).collect();
-            if let [_ts, key] = split[..] {
-                let key_string = key.to_string();
-                match records.entry(key_string.clone()) {
-                    Entry::Vacant(_) => {
-                        records.insert(key_string, v);
-                    }
-                    Entry::Occupied(entry) => {
-                        if entry.get().is_before(&v) {
-                            records.insert(key_string, v);
+        self.backend
+            .get_all::<serde_json::Value>(&self.table)?
+            .into_iter()
+            .for_each(|(k, v)| {
+                serde_json::from_value(v)
+                    .map(|v| {
+                        let split: Vec<&str> = k.split(WAL_KEY_SEP).collect();
+                        if let [_ts, key] = split[..] {
+                            let key_string = key.to_string();
+                            match records.entry(key_string.clone()) {
+                                Entry::Vacant(_) => {
+                                    records.insert(key_string, v);
+                                }
+                                Entry::Occupied(entry) => {
+                                    if entry.get().is_before(&v) {
+                                        records.insert(key_string, v);
+                                    }
+                                }
+                            }
                         }
-                    }
-                }
-            }
-        });
+                    })
+                    .ok();
+            });
         Ok(records)
     }
 
     pub fn get_all<T: DeserializeOwned>(&self) -> Result<Vec<(i64, (String, T))>> {
-        let v = self.backend.get_all::<T>(&self.table)?;
+        let v = self.backend.get_all::<serde_json::Value>(&self.table)?;
         let res = v
             .into_iter()
-            .map(|(k, v)| {
+            .filter_map(|(k, v)| {
                 let (t, k) = match k.split_once(WAL_KEY_SEP) {
                     Some((ts_str, key)) => (ts_str.parse::<i64>().unwrap(), key.to_string()),
                     None => (0i64, k),
                 };
-                (t, (k, v))
+                serde_json::from_value(v).map(|vt| (t, (k, vt))).ok()
             })
             .collect();
         Ok(res)
