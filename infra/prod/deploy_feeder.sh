16a1d7f40319ee64d9b6dc7695cac18abb27f320
Guillaume Balaine <igosuki@gmail.com>
2021-10-27 11:56:04 +0200

    trader: no more feeder to data

--- infra/prod/deploy_feeder.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-BASEDIR=$(dirname "$0")
-
-export OBJC_DISABLE_INITIALIZE_FORK_SAFETY=YES
-
-ansible-playbook -i $BASEDIR/inventory.yml $BASEDIR/playbooks/feeder_deploy.yml -e "INVENTORY_HOST=trader FEEDER_CONFIG_FILE=feeder_prod.yaml"
