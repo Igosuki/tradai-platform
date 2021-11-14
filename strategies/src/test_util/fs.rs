e65e6afd3142ef26bdd43eb6010919daa1955ebe
Guillaume Balaine <igosuki@gmail.com>
2021-11-14 22:54:21 +0100

    trader: refactor plot drawing to use plotly

---
+++ strategies/src/test_util/fs.rs
@@ -0,0 +1,11 @@
+use std::path::PathBuf;
+
+pub fn copy_file(from: &str, to: &str) {
+    let pb = PathBuf::from(from);
+    let pbt = PathBuf::from(to);
+    let pbt_parent = pbt.parent().unwrap();
+    std::fs::create_dir_all(pbt_parent).unwrap();
+    std::fs::File::create(to).unwrap();
+    let copied = std::fs::copy(std::fs::canonicalize(pb).unwrap(), to);
+    assert!(copied.is_ok(), "{}", format!("{:?} : {}", copied, from));
+}
