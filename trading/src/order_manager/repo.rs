802afa4a2dc61af1ee357dc129d149ba58765cad
Guillaume Balaine <igosuki@gmail.com>
2021-11-22 20:53:08 +0100

    move order details repo to its own file

---
+++ trading/src/order_manager/repo.rs
@@ -0,0 +1,23 @@
+use super::error::*;
+use crate::order_manager::types::OrderDetail;
+use db::{Storage, StorageExt};
+use ext::ResultExt;
+use std::sync::Arc;
+
+pub(super) static ORDERS_TABLE: &str = "orders";
+
+#[derive(Debug, Clone)]
+pub struct OrderRepository {
+    db: Arc<dyn Storage>,
+}
+
+impl OrderRepository {
+    pub(crate) fn new(db: Arc<dyn Storage>) -> Self { Self { db } }
+
+    pub(crate) fn get(&self, id: &str) -> Result<OrderDetail> { self.db.get(ORDERS_TABLE, id).err_into() }
+
+    #[tracing::instrument(skip(self), level = "info")]
+    pub(crate) fn put(&self, order: OrderDetail) -> Result<()> {
+        self.db.put(ORDERS_TABLE, &order.id.clone(), order).err_into()
+    }
+}
