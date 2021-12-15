03283c33008c7cef98c88728fc802f7d974ddab4
Guillaume Balaine <igosuki@gmail.com>
2021-12-15 14:44:40 +0100

    [backtest]: Use path partitions now that datafusion supports it

--- backtest/src/datafusion_util.rs
+++ backtest/src/datafusion_util.rs
@@ -1,6 +1,12 @@
 use datafusion::arrow::array::{ArrayRef, StructArray};
 use datafusion::arrow::datatypes::Field;
 use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::datasource::file_format::avro::AvroFormat;
+use datafusion::datasource::file_format::csv::CsvFormat;
+use datafusion::datasource::file_format::parquet::ParquetFormat;
+use datafusion::datasource::file_format::FileFormat;
+use itertools::Itertools;
+use std::sync::Arc;
 
 pub fn get_col_as<'a, T: 'static>(sa: &'a StructArray, name: &str) -> &'a T {
     sa.column_by_name(name).unwrap().as_any().downcast_ref::<T>().unwrap()
@@ -15,4 +21,24 @@ pub fn to_struct_array(batch: &RecordBatch) -> StructArray {
         .map(|t| (t.0.clone(), t.1.clone()))
         .collect::<Vec<(Field, ArrayRef)>>()
         .into()
+}
+
+pub fn df_format(format: String) -> (&'static str, Arc<dyn FileFormat>) {
+    match format.to_lowercase().as_str() {
+        "avro" => ("avro", Arc::new(AvroFormat::default())),
+        "parquet" => ("parquet", Arc::new(ParquetFormat::default())),
+        "csv" => ("csv", Arc::new(CsvFormat::default())),
+        _ => unimplemented!(),
+    }
+}
+
+pub fn where_clause<S: std::fmt::Display, I: Iterator<Item = S>>(iter: &mut I) -> String
+where
+    I: std::iter::ExactSizeIterator,
+{
+    if iter.is_empty() {
+        String::new()
+    } else {
+        "where ".to_string() + iter.join(" and ").as_str()
+    }
 }
--- backtest/src/datasources/orderbook/sampled_source.rs
+++ backtest/src/datasources/orderbook/sampled_source.rs
@@ -1,30 +1,49 @@
-use std::collections::HashSet;
-use std::time::Instant;
-
+use crate::datafusion_util::{df_format, where_clause};
 use datafusion::arrow::array::StringArray;
 use datafusion::arrow::record_batch::RecordBatch;
+use datafusion::datasource::listing::ListingOptions;
 use datafusion::execution::context::ExecutionContext;
+use futures::StreamExt;
+use std::collections::HashSet;
+use std::fmt::Debug;
+use std::path::Path;
+use std::time::Instant;
 use tokio_stream::Stream;
 
 use crate::error::*;
 
 /// Read partitions as sampled order books
-pub fn sampled_orderbooks_df(partitions: HashSet<String>, format: String) -> impl Stream<Item = RecordBatch> + 'static {
+pub fn sampled_orderbooks_df<P: 'static + AsRef<Path> + Debug>(
+    partitions: HashSet<(P, Vec<(&'static str, String)>)>,
+    format: String,
+) -> impl Stream<Item = RecordBatch> + 'static {
     dbg!(&partitions);
-    stream! {
-        for partition in partitions {
-            let ctx = ExecutionContext::new();
-            ctx.clone()
-                .sql(&format!(
-                    "CREATE EXTERNAL TABLE order_books STORED AS {format} LOCATION '{partition}';",
-                    partition = &partition,
-                    format = format
-                ))
-                .await.unwrap();
+    let s = partitions.into_iter().map(move |(base_path, partition)| {
+        let format = format.clone();
+        let base_path = base_path.as_ref().to_str().unwrap_or("").to_string();
+
+        stream! {
             let now = Instant::now();
+            let mut ctx = ExecutionContext::new();
+            let (ext, file_format) = df_format(format);
+            let listing_options = ListingOptions {
+                file_extension: ext.to_string(),
+                format: file_format,
+                table_partition_cols: partition.iter().map(|p| p.0.to_string()).collect(),
+                collect_stat: true,
+                target_partitions: 8,
+            };
+            ctx.register_listing_table(
+                "order_books",
+                &format!("file://{}", base_path),
+                listing_options,
+                None,
+            )
+            .await.unwrap();
+            let where_clause = where_clause(&mut partition.iter().map(|p| format!("{}={}", p.0, p.1)));
             let df = ctx
                 .clone()
-                .sql("select pr, to_timestamp_millis(event_ms) as event_ts, asks, bids from order_books order by event_ms asc")
+                .sql(&format!("select xch, pr, to_timestamp_millis(event_ms) as event_ts, asks, bids from order_books {where_clause} order by event_ms asc", where_clause = &where_clause))
                 .await
                 .unwrap();
             let collected = df.execute_stream().await.unwrap();
@@ -33,13 +52,15 @@ pub fn sampled_orderbooks_df(partitions: HashSet<String>, format: String) -> imp
             }
             let elapsed = now.elapsed();
             info!(
-                "Read records for {} in {}.{}s",
+                "Read records in {} for {:?} in {}.{}s",
+                base_path,
                 partition,
                 elapsed.as_secs(),
                 elapsed.subsec_millis()
             );
         }
-    }
+    });
+    tokio_stream::iter(s).flatten()
 }
 
 /// Find all distinct pairs in the sampled orderbook partitions
@@ -92,4 +113,43 @@ pub async fn sampled_orderbooks_pairs(
         records.extend_from_slice(result.unwrap().as_slice());
     }
     Ok(records)
+}
+
+#[cfg(test)]
+mod test {
+    use datafusion::datasource::file_format::avro::AvroFormat;
+    use datafusion::datasource::listing::ListingOptions;
+    use datafusion::prelude::ExecutionContext;
+    use std::path::PathBuf;
+    use std::sync::Arc;
+
+    // TODO: make some small sample files to test this out instead of production files
+    #[tokio::test]
+    #[ignore]
+    async fn test_read_df() -> datafusion::error::Result<()> {
+        let mut ctx = ExecutionContext::new();
+        let partition = PathBuf::from(env!("COINDATA_CACHE_DIR")).join("data");
+        let listing_options = ListingOptions {
+            file_extension: "avro".to_string(),
+            format: Arc::new(AvroFormat::default()),
+            table_partition_cols: vec!["xch".to_string(), "chan".to_string(), "dt".to_string()],
+            collect_stat: true,
+            target_partitions: 8,
+        };
+        ctx.register_listing_table(
+            "order_books",
+            &format!("file://{}", partition.to_str().unwrap()),
+            listing_options,
+            None,
+        )
+        .await?;
+
+        let df2 = ctx
+            .clone()
+            .sql("select xch, pr, to_timestamp_millis(event_ms) as event_ts, asks, bids from order_books where dt='20211129' and xch='Binance' and chan='1mn_order_books' order by event_ts asc LIMIT 100")
+            .await?;
+        let batch = df2.collect().await?;
+        dbg!(batch);
+        Ok(())
+    }
 }
