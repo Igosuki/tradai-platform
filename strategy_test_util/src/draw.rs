0797f67372eecd49dd8a1dbe5a7ab997fd30cfcf
Guillaume Balaine <igosuki@gmail.com>
2021-12-28 13:46:24 +0100

    [trader][python]: Change draw entries to allow using closures in draw_line_plot and from python

--- strategy_test_util/src/draw.rs
+++ strategy_test_util/src/draw.rs
@@ -1,4 +1,5 @@
 use std::error::Error;
+use std::sync::Arc;
 
 use chrono::{DateTime, Utc};
 use multimap::MultiMap;
@@ -7,26 +8,30 @@ use plotly::{Plot, Scatter};
 
 use util::time::now_str_files;
 
-pub type StrategyEntry<'a, T> = (&'a str, fn(&T) -> Vec<(&'a str, f64)>);
+pub type StrategyEntry<T, S> = (S, StrategyEntryFnRef<T, S>);
+pub type StrategyEntryFnRef<T, S> = Arc<dyn Fn(&T) -> Vec<(S, f64)> + Send + Sync>;
 
 pub trait TimedEntry {
     fn time(&self) -> DateTime<Utc>;
 }
 
-pub fn draw_line_plot<T: TimedEntry>(
+pub fn draw_line_plot<'a, T: 'a + TimedEntry + Clone, S2>(
     module_path: &str,
-    data: Vec<T>,
-    entries: &[StrategyEntry<'_, T>],
-) -> std::result::Result<String, Box<dyn Error>> {
+    data: &'a [T],
+    entries: &[StrategyEntry<T, S2>],
+) -> std::result::Result<String, Box<dyn Error>>
+where
+    S2: ToString + Sized,
+{
     let graph_dir = format!("{}/graphs", module_path);
     std::fs::create_dir_all(&graph_dir).unwrap();
     let out_file = format!("{}/plot_{}.html", graph_dir, now_str_files());
     let mut plot = Plot::new();
     let time: Vec<DateTime<Utc>> = data.iter().map(|x| x.time()).collect();
     let entry_count = entries.len();
-    for (i, line_specs) in entries.iter().enumerate() {
-        let mut lines = MultiMap::new();
-        for v in data.iter().map(line_specs.1).flatten() {
+    for (i, (_entry_name, entry_fn)) in entries.iter().enumerate() {
+        let mut lines: MultiMap<String, f64> = MultiMap::new();
+        for v in data.iter().map(|v| (entry_fn.as_ref())(v)).flatten() {
             lines.insert(v.0.to_string(), v.1);
         }
         for (_, (line_name, y)) in lines.into_iter().enumerate() {
