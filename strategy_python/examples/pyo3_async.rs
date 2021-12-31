604a61451f715fe666611e8e74e540090a567829
Guillaume Balaine <igosuki@gmail.com>
2021-12-31 13:39:42 +0100

    [trader][python]: temporary fix to get the event loop and spawn a task where a python async is awaited forever

--- strategy_python/examples/pyo3_async.rs
+++ strategy_python/examples/pyo3_async.rs
@@ -22,3 +22,49 @@ async def call_me():
 
     Ok(())
 }
+
+// Dump
+//
+// fn dump1() {
+//     let locals = Python::with_gil(|py| {
+//         let asyncio = py.import("asyncio").unwrap();
+//         let event_loop = asyncio.call_method0("get_event_loop").unwrap_or_else(|_| {
+//             let event_loop = asyncio.call_method0("new_event_loop").unwrap();
+//             asyncio.call_method1("set_event_loop", (event_loop,)).unwrap();
+//             event_loop
+//         });
+//         pyo3_asyncio::TaskLocals::new(PyObject::from(event_loop).as_ref(py))
+//         //pyo3_asyncio::tokio::get_current_locals(py)
+//     });
+//     let event_loop = Python::with_gil(|py| {
+//         let asyncio = py.import("asyncio").unwrap();
+//         asyncio
+//             .call_method0("get_event_loop")
+//             .unwrap_or_else(|_| {
+//                 let event_loop = asyncio.call_method0("new_event_loop").unwrap();
+//                 asyncio.call_method1("set_event_loop", (event_loop,)).unwrap();
+//                 event_loop
+//             })
+//             .into_py(py)
+//     });
+// }
+//
+// async fn dump2() {
+//     let py_fut_r = tokio::task::spawn_blocking(move || {
+//         tokio::task::block_in_place(move || {
+//             tokio::runtime::Handle::current().block_on(async move {
+//                 pyo3_asyncio::tokio::scope_local(locals.clone(), async move {
+//                     Python::with_gil(|py| {
+//                         let py1 = inner.call_method1(py, "eval", (0.0,))?;
+//                         let coro = py1.as_ref(py);
+//                         pyo3_asyncio::tokio::into_future(coro)
+//                     })?
+//                     .await
+//                 })
+//                 .await
+//             })
+//         })
+//     })
+//     .await
+//     .unwrap();
+// }
