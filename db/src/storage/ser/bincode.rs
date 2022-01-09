4a86f4a568e362fcf5bbfe07988f1adae9283180
Guillaume Balaine <igosuki@gmail.com>
2022-01-09 22:57:56 +0100

    [trader] bincode serialization for KV Storage

---
+++ db/src/storage/ser/bincode.rs
@@ -0,0 +1,129 @@
+use crate::error::*;
+use crate::storage::{BatchOperation, BatchOperationSer};
+use crate::Storage;
+use ext::ResultExt;
+use serde::de::DeserializeOwned;
+use serde::Serialize;
+
+pub trait BincodeStorageExt {
+    fn put<K, V>(&self, table: &str, key: K, value: V) -> Result<()>
+    where
+        K: AsRef<[u8]>,
+        V: Serialize;
+
+    fn batch<K>(&self, values: &[BatchOperationSer<'_, K>]) -> Result<()>
+    where
+        K: AsRef<[u8]>;
+
+    fn get<K, V>(&self, table: &str, key: K) -> Result<V>
+    where
+        K: AsRef<[u8]>,
+        V: DeserializeOwned;
+
+    fn get_ranged<F, V>(&self, table: &str, from: F) -> Result<Vec<V>>
+    where
+        F: AsRef<[u8]>,
+        V: DeserializeOwned;
+
+    fn get_range<F, F2, V>(&self, table: &str, from: F, to: F2) -> Result<Vec<(String, V)>>
+    where
+        F: AsRef<[u8]>,
+        F2: AsRef<[u8]>,
+        V: DeserializeOwned;
+
+    fn get_all<V>(&self, table: &str) -> Result<Vec<(Box<[u8]>, V)>>
+    where
+        V: DeserializeOwned;
+
+    fn delete<K>(&self, table: &str, key: K) -> Result<()>
+    where
+        K: AsRef<[u8]>;
+
+    fn delete_range<K>(&self, table: &str, from: K, to: K) -> Result<()>
+    where
+        K: AsRef<[u8]>;
+}
+
+impl<T: Storage + ?Sized> BincodeStorageExt for T {
+    fn put<K, V>(&self, table: &str, key: K, value: V) -> Result<()>
+    where
+        K: AsRef<[u8]>,
+        V: Serialize,
+    {
+        let serialized = bincode::serialize::<V>(&value)?;
+        self._put(table, key.as_ref(), serialized.as_slice())
+    }
+
+    fn batch<K>(&self, values: &[BatchOperationSer<K>]) -> Result<()>
+    where
+        K: AsRef<[u8]>,
+    {
+        let vec: Vec<BatchOperation> = values
+            .iter()
+            .map(|(t, k, v)| {
+                (
+                    *t,
+                    k.as_ref(),
+                    v.as_ref().map(|v| bincode::serialize(v.as_ref()).unwrap()),
+                )
+            })
+            .collect();
+        self._batch(&vec)
+    }
+
+    fn get<K, V>(&self, table: &str, key: K) -> Result<V>
+    where
+        K: AsRef<[u8]>,
+        V: DeserializeOwned,
+    {
+        let record = self._get(table, key.as_ref())?;
+        Ok(bincode::deserialize(record.as_slice())?)
+    }
+
+    fn get_ranged<F, V>(&self, table: &str, from: F) -> Result<Vec<V>>
+    where
+        F: AsRef<[u8]>,
+        V: DeserializeOwned,
+    {
+        let items = self._get_ranged(table, from.as_ref())?;
+        items.iter().map(|v| bincode::deserialize(v).err_into()).collect()
+    }
+
+    fn get_range<F, F2, V>(&self, table: &str, from: F, to: F2) -> Result<Vec<(String, V)>>
+    where
+        F: AsRef<[u8]>,
+        F2: AsRef<[u8]>,
+        V: DeserializeOwned,
+    {
+        let items = self._get_range(table, from.as_ref(), to.as_ref())?;
+        items
+            .into_iter()
+            .map(|(k, v)| bincode::deserialize(&v).err_into().map(|d| (k, d)))
+            .collect()
+    }
+
+    fn get_all<V>(&self, table: &str) -> Result<Vec<(Box<[u8]>, V)>>
+    where
+        V: DeserializeOwned,
+    {
+        let items = self._get_all(table)?;
+        items
+            .into_iter()
+            .map(|(k, v)| bincode::deserialize::<V>(&v).map(|v| (k, v)).err_into())
+            .collect()
+    }
+
+    fn delete<K>(&self, table: &str, key: K) -> Result<()>
+    where
+        K: AsRef<[u8]>,
+    {
+        self._delete(table, key.as_ref())
+    }
+
+    fn delete_range<K>(&self, table: &str, from: K, to: K) -> Result<()>
+    where
+        K: AsRef<[u8]>,
+    {
+        self._delete_range(table, from.as_ref(), to.as_ref())
+    }
+}
